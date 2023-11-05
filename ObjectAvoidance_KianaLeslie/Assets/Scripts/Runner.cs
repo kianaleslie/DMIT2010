c98b1dd950e800ca8a4a5f1f25681cdbce3f6ff3
kiana <kianaleslie25@gmail.com>
2023-11-05 01:47:31 -0600

    basic environment set up

---
+++ ObjectAvoidance_KianaLeslie/Assets/Scripts/Runner.cs
@@ -0,0 +1,105 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Runner : MonoBehaviour
+{
+    [SerializeField] GameObject target;
+    [SerializeField] float movementSpeed = 3.0f;
+    [SerializeField] float forwardDist = 1.0f;
+    [SerializeField]  float sideDist = 3.0f;
+
+    RaycastHit hit;
+    bool isLeft; 
+    bool isRight;
+   
+    void Start()
+    {
+
+    }
+    void Update()
+    {
+        CheckTargets();
+
+        AvoidWalls();
+
+        transform.Translate(Vector3.forward * movementSpeed * Time.deltaTime);
+    }
+    //private void OnTriggerEnter(Collider other)
+    //{
+    //    Rigidbody rb = other.GetComponentInParent<Rigidbody>();
+
+    //    if (rb != null)
+    //    {
+    //        if (rb.tag == "Collectable")
+    //        {
+    //            target = other.gameObject;
+    //        }
+    //    }
+    //}
+    void AvoidWalls()
+    {
+        if (Physics.BoxCast(transform.position, new Vector3(0.5f, 0.5f, 0.5f), transform.forward, out hit, Quaternion.identity, forwardDist))
+        {
+            if (hit.transform.gameObject.tag == "Wall")
+            {
+
+                transform.rotation = Quaternion.LookRotation(Vector3.RotateTowards(transform.forward, -hit.normal, 1, 1));
+
+                // Rotate based on what is to the sides
+                isLeft = Physics.Raycast(transform.position, -transform.right, sideDist);
+                isRight = Physics.Raycast(transform.position, transform.right, sideDist);
+
+                if (isLeft && isRight)
+                {
+                    transform.Rotate(Vector3.up, 180);
+                }
+                else if (isLeft && !isRight)
+                {
+                    transform.Rotate(Vector3.up, 90);
+                }
+                else if (!isLeft && isRight)
+                {
+                    transform.Rotate(Vector3.up, -90);
+                }
+                else
+                {
+                    if (Random.Range(1, 3) == 1)
+                    {
+                        transform.Rotate(Vector3.up, 90);
+                    }
+                    else
+                    {
+                        transform.Rotate(Vector3.up, -90);
+                    }
+                }
+            }
+        }
+    }
+    void CheckTargets()
+    {
+        if (target != null)
+        {
+            if (target.gameObject.activeSelf)
+            {
+                if (Physics.BoxCast(transform.position, new Vector3(0.5f, 0.5f, 0.5f), target.transform.position - transform.position, out hit, Quaternion.identity))
+                {
+                    if (hit.transform.tag != "Wall")
+                    {
+                        transform.rotation = Quaternion.LookRotation(Vector3.RotateTowards(transform.forward, target.transform.position - transform.position, 1, 1));
+                    }
+
+                    if (Vector3.Distance(target.transform.position, transform.position) < 2.0f)
+                    {
+                        target.SetActive(false);
+                        target = null;
+                    }
+                }
+            }
+            else
+            {
+                target = null;
+            }
+        }
+    }
+}
